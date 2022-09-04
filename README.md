193bbe2563a4abbf0400e13492c1a98c3552eec7
Yolan <yolanother@gmail.com>
2022-09-04 12:57:24 -0700

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SharedStableDiffusionBot
 A simple bot for running stable diffusion via replicate. Users must enter their own replicate tokens with the /replicate command
+
+## Installation
+pip install -r requirements.txt
+Create a file named .env. This text file will be used to store secrets for your development environment. Paste in the following:
+DISCORD_TOKEN=<your-token>
+
+
+## References
+[Replicate Guide](https://replicate.com/blog/build-a-robot-artist-for-your-discord-server-with-stable-diffusion)
---
+++ main.py
@@ -0,0 +1,84 @@
+import json
+import os
+import pickle
+import replicate as rep
+import discord
+
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = discord.Bot()
+
+def save_user_data():
+    with open("sdbot-userdata.pkl", "wb") as f:
+        pickle.dump(userdata, f)
+
+def load_user_data():
+    if not os.path.exists("sdbot-userdata.pkl"):
+        return dict()
+    with open("sdbot-userdata.pkl", "rb") as f:
+        try:
+            return pickle.load(f)
+        except:
+            return dict()
+
+userdata = load_user_data()
+
+def get_user_data(user):
+    if user not in userdata:
+        userdata[user] = dict()
+    return userdata[user]
+
+def get_token(userId):
+    user = get_user_data(userId)
+    encrypt = Fernet(user["key"])
+    return encrypt.decrypt(user["token"]).decode()
+
+
+
+@bot.slash_command()
+async def replicate(ctx, *, token):
+    user = get_user_data(ctx.author.id)
+    if "key" not in user:
+        user["key"] = Fernet.generate_key()
+
+    encrypt = Fernet(user["key"])
+    user["token"] = encrypt.encrypt(token.encode())
+    save_user_data()
+
+
+async def logo(ctx, *, prompt):
+    """Generate a logo from a text prompt using the stable-diffusion model"""
+    await ctx.respond(f"“{prompt}”\n> Generating...")
+
+    c = rep.Client(api_token=get_token(ctx.author.id))
+    model = c.models.get("laion-ai/erlich")
+    prediction = model.predict(prompt=prompt)
+    image = prediction.first().image
+
+    await ctx.respond(content=f"“{prompt}”\n{image}")
+
+@bot.slash_command()
+async def dream(ctx, *, prompt, width=512, height=512):
+    """Generate an image from a text prompt using the stable-diffusion model"""
+    msg = await ctx.respond(f"“{prompt}”\n> Generating...")
+
+    try:
+        c = rep.Client(api_token=get_token(ctx.author.id))
+        model = c.models.get("stability-ai/stable-diffusion")
+        result = model.predict(prompt=prompt, width=width, height=height)
+        image = result[0]
+
+        print (f"“{prompt}”\n{image}")
+        await ctx.respond(content=f"“{prompt}”\n{image}")
+    except Exception as e:
+        print(f"“{prompt}”\n> {e}")
+        await ctx.respond(content=f"“{prompt}”\n> {e}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    bot.run(os.getenv("DISCORD_TOKEN"))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
